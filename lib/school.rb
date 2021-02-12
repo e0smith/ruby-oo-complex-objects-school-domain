2be6036014d70ef77744f3ad1d8ee8016e9d162e
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-02-12 15:56:29 -0500

    commit tree d5f342906710a0ade9740a15675e8ac77420ea20

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+require "pry"
+class School
+    attr_accessor :school, :name, :roster
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else 
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+    def grade(grade)
+        self.roster[grade]
+    end
+    def sort
+        self.roster.select do |grade, name|
+            name.sort!
+        end
+    end
+end
